63ce05f36161f2a969c78b5c17282e2e037f6337
2wndrhs <2wndrhs@gmail.com>
2024-10-04 04:50:39 +0900

    feat: 커맨드라인 인자 에러 처리 기능 추가

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
-use std::{env, fs};
+use std::{env, fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        // 에러가 발생할 경우 클로저 내부의 코드가 실행됨
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
@@ -20,10 +24,14 @@ struct Config {
 
 impl Config {
     // String 슬라이스(연속된 String 요소를 참조)를 인자로 받음
-    fn new(args: &[String]) -> Config {
+    fn build(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Config { query, file_path }
+        Ok(Config { query, file_path })
     }
 }
