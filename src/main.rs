0127097096f15cb29d5b17e7f0e04d45a84390d9
2wndrhs <2wndrhs@gmail.com>
2024-10-04 13:06:15 +0900

    fix: 에러가 표준 출력이 아닌 표준 에러에 출력되도록 수정

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,12 @@ fn main() {
 
     let config = Config::build(&args).unwrap_or_else(|err| {
         // 에러가 발생할 경우 클로저 내부의 코드가 실행됨
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
