2a46c5f50da89809b850f4e64ba514e9ae8527eb
2wndrhs <2wndrhs@gmail.com>
2024-10-04 12:37:00 +0900

    feat: `search()` 함수 구현

--- src/lib.rs
+++ src/lib.rs
@@ -23,12 +23,25 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
+    // search 함수에서 반환된 문자열 슬라이스 벡터를 반복하여 각 라인을 출력
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
+
     Ok(())
 }
 
 // 라이프타임을 명시하여 반환되는 벡터의 문자열 슬라이스가 contents 문자열 슬라이스의 참조임을 나타냄
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    vec![]
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
 }
 
 #[cfg(test)]
@@ -43,6 +56,6 @@ Rust:
 safe, fast, productive.
 Pick three.";
 
-        assert_eq!(vec!["safe, fast, productive"], search(query, contents));
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 }
